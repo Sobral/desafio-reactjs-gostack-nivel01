fe96a185d1cfd4610714730262ffaecad7c022a2
Luciano Sobral <sobral.luciano@gmail.com>
2020-06-19 15:30:40 -0400

    Testes passando com sucesso. :D

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [techs, setTechs] = useState('');
+  const [url, setUrl] = useState('');
+  
+  useEffect(() => {
+  api.get('repositories').then(response => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post('repositories', {title, techs, url});
+    setTechs('');
+    setTitle('');
+    setUrl('');
+    setRepositories([...repositories, repository.data]); 
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    api.delete(`repositories/${id}`).then(
+    setRepositories(repositories.filter(repository => repository.id !== id)))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => 
+   
+        <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
-      </ul>
+        
 
+        )}
+
+      </ul>
+      <input type="text" name="title" placeholder="title" value={title} onChange={e=>setTitle(e.target.value)}/>
+      <input type="text" name="url" placeholder="url" value={url} onChange={e=>setUrl(e.target.value)}/>
+      <input type="text" name="techs" placeholder="techs" value={techs} onChange={e => setTechs(e.target.value)}/>
       <button onClick={handleAddRepository}>Adicionar</button>
+
     </div>
   );
 }
